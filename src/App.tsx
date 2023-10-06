6551077136a66cad32118d95c908931122d2dc59
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-05 21:12:20 -0300

    primeiros passos do projeto

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { FileText, GithubLogo, LinkedinLogo } from '@phosphor-icons/react'
+
+function App() {
+  return (
+    <>
+      <h2 className="mx-1 text-xl font-semibold">Oi! Eu sou Alexandre Martins</h2>
+      <section className="flex justify-between items-center">
+        <h1 className="text-6xl font-bold">FRONTEND</h1>
+
+        <div className="flex gap-8">
+          <a className="hover:bg-sky-200 transition-all p-3 rounded-full" href="https://github.com/gafanhotoalexandre">
+            <GithubLogo size={32} />
+          </a>
+          <a className="hover:bg-sky-200 transition-all p-3 rounded-full" href="https://www.linkedin.com/in/alemartins-lima/">
+            <LinkedinLogo size={32} />
+          </a>
+          <a className="hover:bg-sky-200 transition-all p-3 rounded-full" download href="/curriculo.pdf">
+            <FileText size={32} />
+          </a>
+        </div>
+      </section>
+      <h2 className="text-9xl font-bold -mx-2">DEVELOPER</h2>
+
+      <p className="w-[450px] ml-auto text-justify mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur magni nostrum adipisci ex facere ipsa corrupti odit ipsum.</p>
+      {/* <div className="fixed top-0 left-0 w-screen h-screen bg-overlay"></div> */}
+    </>
+  )
+}
+
+export default App
